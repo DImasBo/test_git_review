44ff435551c3ffef665057a90cec2d4caedb58f6
dimasbo <dimasbo.work@gmail.com>
2023-05-10 20:26:36 +0300

    lesson:  OOP імпорт класів

---
+++ lesson_OOP/lessons_package/main.py
@@ -0,0 +1,9 @@
+from utils import Cut, Molotok, C3, C4
+
+c3 = C3()
+m = Molotok()
+
+
+# class PlayGame
+
+# def play_game()
---
+++ lesson_OOP/lessons_package/utils.py
@@ -0,0 +1,20 @@
+
+
+class Molotok:
+    l = 10
+
+class Cut:
+    pass
+
+class C3:
+    def m1(self):
+        pass
+
+    def m2(self):
+        pass
+
+class C4:
+
+    def m3(self):
+        pass
+
