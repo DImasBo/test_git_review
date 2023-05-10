add841dfed8ae69569930a9a2ba3139a6cc8802c
dimasbo <dimasbo.work@gmail.com>
2023-05-10 20:04:02 +0300

    lesson:  OOP інкапсуляція

--- lesson_OOP/second.py
+++ lesson_OOP/second.py
@@ -1,49 +1,89 @@
-# наслідування
+# # наслідування
+# #
+# # class A:
+# #     name = "test"
+# #
+# #     def m(self):
+# #         print("test m")
+# #
+# #
+# # class B(A):
+# #
+# #     def show(self):
+# #         self.m()
+# #         print(self.name)
+# #
+# # b = B()
+# #
+# # b.show()
+#
+#
+# # class Koleso:
+# #     r = 10
+# #
+# # class Motor:
+# #     power = 120
+# #
+# # class Car(Motor, Koleso):
+# #     name = "BMW"
+# #
+# #
+# # c = Car()
+#
 #
 # class A:
-#     name = "test"
 #
-#     def m(self):
-#         print("test m")
+#     def test(self):
+#         print("test A")
 #
 #
-# class B(A):
+# # class B(A):
 #
-#     def show(self):
-#         self.m()
-#         print(self.name)
+#     def test(self):
+#         #super().test()
+#         print("test B")
+#
 #
 # b = B()
 #
-# b.show()
+# b.test()
 
 
-# class Koleso:
-#     r = 10
+#*----------- Інкапсуляція
 #
-# class Motor:
-#     power = 120
+# class A:
+#     t = "t"
+#     __password = "ttesrevsdfvsd"
 #
-# class Car(Motor, Koleso):
-#     name = "BMW"
+#     def test(self):
+#         print("test")
+#
+#     def test(self):
+#         self.__my_secret()
+#         self.__password
+#
+#     def __my_secret(self):
+#         print("я маю секрет ...")
 #
 #
-# c = Car()
-
-
-class A:
-
-    def test(self):
-        print("test A")
-
-
-class B(A):
-
-    def test(self):
-        #super().test()
-        print("test B")
-
-
-b = B()
-
-b.test()
+# a = A()
+#
+# print(a.t)
+# a.test()
+#
+# class P:
+#     _t = "t"
+#
+#     def _test(self):
+#         print("protect")
+#
+#
+# p = P()
+# p._test()
+#
+#
+# print(p._t)
+#
+# p._t = "test12"
+#
+# print(p._t)
