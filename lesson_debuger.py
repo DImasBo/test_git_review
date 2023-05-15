99ab2477027ae9a8b95349b0d2e5d1dd8acdf1ab
dimasbo <dimasbo.work@gmail.com>
2023-05-15 19:44:45 +0300

    lesson: lessons for debug

---
+++ lesson_debuger.py
@@ -0,0 +1,22 @@
+# bug
+
+
+def test(a, b):
+    c = a+b
+    return c
+
+
+def t2():
+    pass
+
+
+print("Hello Wold")
+
+name = "Dmytro"
+a = 10
+b = 5
+
+r = test(a, b)
+
+if r == 15:
+    print("15")
