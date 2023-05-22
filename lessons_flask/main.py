4a53a470cd1423b5a914302fb23e3bb66fce7293
dimasbo <dimasbo.work@gmail.com>
2023-05-22 21:29:45 +0300

    lesson: added template for Blog

--- lessons_flask/main.py
+++ lessons_flask/main.py
@@ -1,31 +1,36 @@
 from flask import Flask, render_template
 
 app = Flask(__name__)
+app.debug = True
 
-name = "Dmytro"
+news_all = [
+    {
+        "header": "Заголовок Новини",
+        "author": "Дмитро Б.",
+        "context": "опис новини",
+        "date": "20.05.2023",
+    },
+]
 
-news_all = {
-    "Погода": "На вулиці ясно температура 22",
-    "Bitcoin": "Біткоїн знову упав!...",
-    "ПризедетЗустріч": "Призедин зустрівся з кимось ...!...",
+user = {
+    "name": "Dmytro",
+    "nikname": "dimas",
+    "age": 22
 }
 
 
 @app.route("/")
 def hello():
-    news_text = ""
-    for key, val in news_all.items():
-        news_text += f'<a href="/news/{key}">{key}</a>'
-    return f"<h1>Hello, {name}</h1>"+ news_text
+    return render_template("index.html", user=user)
 
 
-@app.route("/news/<name_post>")
-def news_wather(name_post):
-    news = news_all.get(name_post)
-    if not news:
-        return "<h1>Такої новини в нас немає :(</h1>"
-    return f"<h1>Новина: {name_post}</h1>" \
-           f"<p>{news}</p>"
+@app.route("/news/<index_post>")
+def news_post(index_post):
+    index_post = int(index_post)
+    if index_post > len(news_all):
+        return "<h1>Новина не знайдена</h1>"
+    news = news_all[index_post]
+    return render_template("post.html", news=news)
 
 
 app.run()
