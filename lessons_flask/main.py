2d88336820c1e0667d2912977492542284d28509
dimasbo <dimasbo.work@gmail.com>
2023-05-22 20:24:28 +0300

    lesson: describe Blog Flask

--- lessons_flask/main.py
+++ lessons_flask/main.py
@@ -4,25 +4,25 @@ app = Flask(__name__)
 
 name = "Dmytro"
 
+news_all = {
+    "Погода": "На вулиці ясно температура 22",
+    "Bitcoin": "Біткоїн знову упав!...",
+    "ПризедетЗустріч": "Призедин зустрівся з кимось ...!...",
+}
+
+
 @app.route("/")
 def hello():
     return f"<h1>Hello, {name}</h1>"
 
 
-@app.route("/about")
-def about():
-    return f"<h1>Hello about</h1>"
-
-
-@app.route("/news")
-def news():
-    return f"<h1>All news</h1>"
-
-
 @app.route("/news/<name_post>")
 def news_wather(name_post):
-
-    return f"<h1>Новина {name_post}</h1>"
+    news = news_all.get(name_post)
+    if not news:
+        return "<h1>Такої новини в нас немає :(</h1>"
+    return f"<h1>Новина: {name_post}</h1>" \
+           f"<p>{news}</p>"
 
 
 app.run()
