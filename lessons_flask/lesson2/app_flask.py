59cc116806b5f11c72102d705fc4a50a484f3f79
dimasbo <dimasbo.work@gmail.com>
2023-05-23 20:20:47 +0300

    lesson: lessons 2 flask form create news

--- lessons_flask/lesson2/app_flask.py
+++ lessons_flask/lesson2/app_flask.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 app.debug = True
@@ -11,6 +11,7 @@ news_list = [
     }
 ]
 
+
 @app.route("/index")
 def hello():
     return "<h1>Hello, World!</h1>"
@@ -22,9 +23,32 @@ def news(index_news):
     if index_news >= len(news_list):
         return "<h1>такої новини немає у списку</h1>"
     news = news_list[index_news]
-    return f"<h1>Новина: {news['name']}</h1>" \
+    return f"Новин на сайті {len(news_list)}" \
+           f"<h1>Новина: {news['name']}</h1>" \
            f"<p>{news['context']}</p>"\
            f"<p> <small>author:{news['author']}</small></p>"
+
+
+@app.route("/form/create/", methods=['GET'])
+def create_news():
+    return """
+<form method="post">
+    <label>заголовок</label>
+    <input name="name" type="text">
+    <label>Опис</label>
+    <input name="context" type="text">
+    <label>Автор</label>
+    <input name="author" type="text">
+    <input type="submit">
+</form>    
+"""
+
+
+@app.route("/form/create/", methods=['POST'])
+def create_news_post():
+    news = request.form.to_dict()
+    news_list.append(news)
+    return "на сайті новин" + str(len(news_list))
 
 
 app.run()
