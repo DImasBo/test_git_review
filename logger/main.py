0928a41ed9e4cd76a00a74e28432d27d08c55e96
Your Name <you@example.com>
2023-05-24 20:10:12 +0300

    add lessons about loggings

--- logger/main.py
+++ logger/main.py
@@ -1,24 +1,37 @@
 import logging
-
+import other
 
 logging.basicConfig(level="DEBUG", filename="mylogs.log",
-                    format="%(asctime)s | %(levelname)s: %(message)s"
+                    format="%(asctime)s | %(name)s  | %(levelname)s: %(message)s"
                     )
 
+logger = logging.getLogger("image")
+logger.setLevel("DEBUG")
 
-def calc(a, b):
+def dowload_image():
+    logger.debug("dowlaod")
+    print("скачую")
+    logger.info("скачався файл")
+    other.test()
+    print("файл скачався")
 
-    c = a+b * 2
-    logging.debug(f"call function calc: {a} and {b} = {c}")
-    return c
-
-
-a = int(input("a:"))
-b = int(input("b:"))
-print(calc(a, b))
-
-try:
-    file = open("config.txt")
-except Exception as e:
-    logging.error("something wrong" + str(e))
-
+dowload_image()
+#
+# def calc(a, b):
+#
+#     c = a+b * 2
+#     logging.debug(f"call function calc: {a} and {b} = {c}")
+#     return c
+#
+#
+# a = int(input("a:"))
+# b = int(input("b:"))
+# print(calc(a, b))
+#
+# try:
+#     file = open("config.txt")
+# except Exception as e:
+#     logging.error("something wrong" + str(e))
+#
+#
+# logging.info("просто інфомрація")
